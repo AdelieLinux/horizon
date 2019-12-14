df550933fb33ad6ed8bbbf7559afa1f6d797ac85
A. Wilcox <AWilcox@Wilcox-Tech.com>
2019-12-14 12:57:43 -0600

    Qt UI: Add splash

--- ui/qt5/main.cc
+++ ui/qt5/main.cc
@@ -11,11 +11,34 @@
  */
 
 #include <QApplication>
+#include <QDialog>
 #include <QIcon>
+#include <QLabel>
 #include <QLibraryInfo>
 #include <QTranslator>
+#include <QVBoxLayout>
 
 #include "horizonwizard.hh"
+
+class WaitDialog : public QDialog {
+public:
+    WaitDialog(QWidget *parent = nullptr);
+    void accept() override;
+    void reject() override;
+};
+
+WaitDialog::WaitDialog(QWidget *parent) : QDialog(parent) {
+   setWindowTitle(tr("Loading..."));
+   QVBoxLayout *layout = new QVBoxLayout;
+   QLabel *descLabel = new QLabel(tr("System Installation is loading.\n\nPlease wait a moment."));
+   descLabel->setAlignment(Qt::AlignCenter);
+   layout->addWidget(descLabel);
+   setLayout(layout);
+}
+
+/* Prevents the user from interacting in any way. */
+void WaitDialog::accept() { return; }
+void WaitDialog::reject() { return; }
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
@@ -30,9 +53,16 @@ int main(int argc, char *argv[]) {
         app.installTranslator(translator);
     }
 
+    WaitDialog d;
+    d.show();
+    app.setOverrideCursor(Qt::WaitCursor);
+    app.processEvents(QEventLoop::AllEvents, 300);
+
     app.setWindowIcon(QIcon(":/horizon-256.png"));
 
     HorizonWizard wizard;
+    d.hide();
+    app.restoreOverrideCursor();
     wizard.show();
 
     return app.exec();
